3212e507c8b7727339112493d5e73371ee9b6ea9
Brandon Lok (Qi Luo) <branlok95@gmail.com>
2025-04-07 17:05:58 -0400

    basic skeleton for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,49 @@
-
+import { SignInButton, SignOutButton, SignUpButton } from '@clerk/nextjs'
+import styles from './page.module.css'
 
 export default async function Home() {
-
+  // TODO - if user already signed in, redirect them to their profile page.
   return (
-    <>
-      <aside>
-        <h2>Watdoing</h2>
+    <div className={styles.wrapper}>
+      <aside className={styles.aside}>
+        <h1>Watdoing</h1>
       </aside>
-      <main>
-        <header>
-          <div>
+      <main className={styles.main}>
+        <header className={styles.header}>
+          <div className={styles.headlineContainer}>
             <span>Stop Dreaming, start doing</span>
             <h1>From a thought to a spark</h1>
             <p>Grow your ideas. Collaborate with creators. A calm space to shape your vision.</p>
           </div>
-          <div>
-            <div>
-              <a href="">Create Acctoun</a>
-              <a href="">Sign in</a>
-            </div>
-            <hr />
+          <div className={styles.previewContainer}>
+            profile here...
           </div>
         </header>
+        <div className={styles.ctaWrapper}>
+          <div>
+
+            <SignUpButton>
+              <button>Create Account</button>
+            </SignUpButton>
+
+            <SignOutButton>
+              <button>Sign Out</button>
+            </SignOutButton>
+            <SignInButton>
+              <button>Sign In</button>
+            </SignInButton>
+          </div>
+        </div>
+        <section className={styles.informationSection}>
+          <div>
+            <ul>
+              <li>Toss out quick thoughts on anything—beyond likes, no judgment, just yours to keep private or share with the world.</li>
+              <li>Mold your ideas with pre-post sketches and post-post polish into something radiant.</li>
+            </ul>
+          </div>
+        </section>
       </main>
-    </>
+    </div>
 
   )
 }
